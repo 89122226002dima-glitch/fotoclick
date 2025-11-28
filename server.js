d0ae84ae1c15b963176706a455cf043ce50d614c
89122226002dima-glitch <89122226002dima@gmail.com>
2025-11-28 22:27:43 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ db.read().then(() => {
 
 const INITIAL_CREDITS = 1;
 const PROMO_CODES = {
-    "521377": { type: 'credits', value: 500, message: "Владелец активировал 500 тестовых кредитов." },
-    "521374": { type: 'credits', value: 500, message: "Владелец активировал 500 тестовых кредитов." }
+    "521373": { type: 'credits', value: 500, message: "Владелец активировал 500 тестовых кредитов." },
+    "FREE": { type: 'credits', value: 12, message: "Вы получили 12 бесплатных кредитов!" }
 };
 
 
